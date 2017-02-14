b57f634a0a268b14c76fe86ccd5c4456866d1abd
NukeVlad <vipvnnru@yahoo.com>
2017-02-14 20:58:27 +0300

    Create admin_ru.php

---
+++ modules/events/language/admin_ru.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+* @Project NUKEVIET 4.x
+* @Author VINADES.,JSC (contact@vinades.vn)
+* @Copyright (C) 2017 VINADES.,JSC. All rights reserved
+* @Language русский язык
+* @License CC BY-SA (http://creativecommons.org/licenses/by-sa/4.0/)
+* @Createdate Feb 13, 2017, 06:42:07 PM
+*/
+
+if (! defined('NV_ADMIN') or ! defined('NV_MAINFILE')) {
+    die( 'Stop!!!' );
+}
+
+$lang_translator['author'] = 'schavlev &#40;vipvnnru@yahoo.com&#41;';
+$lang_translator['createdate'] = 'Sun, 12 Jun 2016 05:02:54 GMT';
+$lang_translator['copyright'] = '@Copyright &#40;C&#41; 2017 schavlev. All rights reserved';
+$lang_translator['info'] = '';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['add'] = 'Добавить';
+$lang_module['list'] = 'Список событий';
+$lang_module['edit'] = 'Редактировать';
+$lang_module['title'] = 'Наименование';
+$lang_module['alias'] = 'Статическая ссылка';
+$lang_module['errorsave'] = 'По некоторым причинам, эти данные не могут быть сохранены';
+$lang_module['function'] = 'Действия';
+$lang_module['order'] = 'Номер';
+$lang_module['status'] = 'Статус';
+$lang_module['content_title'] = 'Имя события';
+$lang_module['content_time_start'] = 'Время начала';
+$lang_module['content_time_end'] = 'Время окончания';
+$lang_module['content_location'] = 'Места';
+$lang_module['content_images'] = 'Фотографии с мероприятия';
+$lang_module['content_hometext'] = 'Краткое описание';
+$lang_module['content_bodytext'] = 'Основной текст';
+$lang_module['content_time'] = 'Продолжительность';
+$lang_module['content_error_title'] = 'Ошибка: имя события не может быть пустым';
+$lang_module['content_error_alias'] = 'Ошибка: статическая ссылка дублируется';
+$lang_module['content_error_time_start'] = 'Ошибка: событие Дата начала';
+$lang_module['content_error_time_end'] = 'Ошибка: время окончания не после времени начала или оставьте поле пустым';
+$lang_module['content_error_hometext'] = 'Ошибка: краткое описание не заполнено';
+$lang_module['content_error_bodytext'] = 'Ошибка: подробное содержание еще не заполнено';
+$lang_module['cat'] = 'Тема';
+$lang_module['cat_title'] = 'Название темы';
+$lang_module['cat_description'] = 'Введение';
+$lang_module['cat_add'] = 'Другие темы';
+$lang_module['cat_edit'] = 'Редактировать тему';
+$lang_module['cat_error_title'] = 'Ошибка: имя события не может быть пустым';
+$lang_module['cat_error_exists'] = 'Ошибка: Название темы дублируется';
