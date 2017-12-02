598c76dae566a7c8caceae1dd40f12a8b50bb8e1
NukeVlad <vipvnnru@yahoo.com>
2017-12-02 15:36:51 +0300

    Add files via upload
    Мною выполнена русификация файлов локализации

---
+++ modules/events/language/admin_ru.php
@@ -0,0 +1,51 @@
+<?php
+
+/**
+* @Project NUKEVIET 4.x
+* @Author VINADES.,JSC <contact@vinades.vn>
+* @Copyright (C) 2017 VINADES.,JSC. All rights reserved
+* @Language русский язык
+* @License CC BY-SA (http://creativecommons.org/licenses/by-sa/4.0/)
+* @Createdate Nov 27, 2017, 01:36:18 PM
+*/
+
+if (!defined('NV_ADMIN') or !defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$lang_translator['author'] = 'PHAN TAN DUNG &#40;phantandung92@gmail.com&#41;';
+$lang_translator['createdate'] = 'Sun, 12 Jun 2016 05:02:54 GMT';
+$lang_translator['copyright'] = '@Copyright &#40;C&#41; 2017 schavlev. http&#x3A;&#x002F;&#x002F;nukeviet.ru&#x002F;';
+$lang_translator['info'] = '@translator ru schavlev &#40;info@nukevit.ru&#41; nukeviet.ru';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['add'] = 'Добавить';
+$lang_module['list'] = 'Список событий';
+$lang_module['edit'] = 'Редактировать';
+$lang_module['title'] = 'Наименование';
+$lang_module['alias'] = 'Статическая ссылка';
+$lang_module['errorsave'] = 'По некоторым причинам, эти данные не могут быть сохранены';
+$lang_module['function'] = 'Действия';
+$lang_module['order'] = 'Номер';
+$lang_module['status'] = 'Статус';
+$lang_module['content_title'] = 'Имя события';
+$lang_module['content_time_start'] = 'Время начала';
+$lang_module['content_time_end'] = 'Время окончания';
+$lang_module['content_location'] = 'Места';
+$lang_module['content_images'] = 'Фотографии с мероприятия';
+$lang_module['content_hometext'] = 'Краткое описание';
+$lang_module['content_bodytext'] = 'Основной текст';
+$lang_module['content_time'] = 'Продолжительность';
+$lang_module['content_error_title'] = 'Ошибка: имя события не может быть пустым';
+$lang_module['content_error_alias'] = 'Ошибка: статическая ссылка дублируется';
+$lang_module['content_error_time_start'] = 'Ошибка: событие Дата начала';
+$lang_module['content_error_time_end'] = 'Ошибка: время окончания не после времени начала или оставьте поле пустым';
+$lang_module['content_error_hometext'] = 'Ошибка: краткое описание не заполнено';
+$lang_module['content_error_bodytext'] = 'Ошибка: подробное содержание еще не заполнено';
+$lang_module['cat'] = 'Категория';
+$lang_module['cat_title'] = 'Наименование категории';
+$lang_module['cat_description'] = 'Описание';
+$lang_module['cat_add'] = 'Создать категорию';
+$lang_module['cat_edit'] = 'Редактировать категорию';
+$lang_module['cat_error_title'] = 'Ошибка: имя события не может быть пустым';
+$lang_module['cat_error_exists'] = 'Ошибка: Название темы дублируется';
---
+++ modules/events/language/ru.php
@@ -0,0 +1,42 @@
+<?php
+
+/**
+* @Project NUKEVIET 4.x
+* @Author VINADES.,JSC <contact@vinades.vn>
+* @Copyright (C) 2017 VINADES.,JSC. All rights reserved
+* @Language русский язык
+* @License CC BY-SA (http://creativecommons.org/licenses/by-sa/4.0/)
+* @Createdate Nov 27, 2017, 01:36:18 PM
+*/
+
+if (!defined('NV_MAINFILE')) {
+    die('Stop!!!');
+}
+
+$lang_translator['author'] = 'PHAN TAN DUNG &#40;phantandung92@gmail.com&#41;';
+$lang_translator['createdate'] = 'Sun, 12 Jun 2016 05:02:54 GMT';
+$lang_translator['copyright'] = '@Copyright &#40;C&#41; 2017 schavlev. http&#x3A;&#x002F;&#x002F;nukeviet.ru&#x002F;';
+$lang_translator['info'] = '@translator ru schavlev &#40;info@nukevit.ru&#41; nukeviet.ru';
+$lang_translator['langtype'] = 'lang_module';
+
+$lang_module['cat'] = 'Категория';
+$lang_module['location'] = 'Место мероприятия';
+$lang_module['search_q'] = 'Введите поисковое слово';
+$lang_module['search'] = 'Искать';
+$lang_module['search_title'] = 'Найти события';
+$lang_module['search_error_q'] = 'Нужно ввести запрос в поиске';
+$lang_module['search_all_cat'] = 'По всем категориям';
+$lang_module['search_by_day'] = 'Дата события';
+$lang_module['search_after_day'] = 'События после:';
+$lang_module['search_before_day'] = 'События до даты:';
+$lang_module['search_being_day'] = 'Текущие события';
+$lang_module['event_future'] = 'Предстоящие события';
+$lang_module['event_now'] = 'Текущие события';
+$lang_module['event_history'] = 'Завершившиеся события';
+$lang_module['week_T2'] = 'Пн';
+$lang_module['week_T3'] = 'Вт';
+$lang_module['week_T4'] = 'Ср';
+$lang_module['week_T5'] = 'Чт';
+$lang_module['week_T6'] = 'Пт';
+$lang_module['week_T7'] = 'Сб';
+$lang_module['week_CN'] = 'Вс';
